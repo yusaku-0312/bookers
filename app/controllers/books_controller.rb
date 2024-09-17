7bc25c06876817aa03b67c19e580453cc4c26625
yusaku-0312 <161700710+yusaku-0312@users.noreply.github.com>
2024-09-17 02:38:50 +0000

    [Modify]最終版

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,13 @@ class BooksController < ApplicationController
   end
   
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = 'Book was successfully created.'
       redirect_to book_path(@book.id)
     else
-      render '/books'
+      render :index
     end
   end
   
@@ -26,19 +27,20 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
    
-    if book.update(book_params)
+    if @book.update(book_params)
       flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
     end
   end
   
   def destroy
     book = Book.find(params[:id])
-    if book.destroy
-      flash[:notice] = 'Book was successfully created.'
-    end
+    flash[:notice] = 'Book was successfully created.'
+    book.destroy
     redirect_to '/books'
   end
   
