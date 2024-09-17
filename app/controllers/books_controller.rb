ff576b662a600d04952da23eadb9944810bd725a
yusaku-0312 <161700710+yusaku-0312@users.noreply.github.com>
2024-09-17 00:26:45 +0000

    [Finish]destroy機能]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,12 +24,24 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-    #updateした時にフラッシュメッセージ表示されていない
+   
+    if book.update(book_params)
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    end
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = 'Book was successfully created.'
+    end
+    redirect_to '/books'
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
